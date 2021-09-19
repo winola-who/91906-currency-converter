c127fd296dba9c850c87f5fa6729e378f536bd3f
winola-who <68686441+winola-who@users.noreply.github.com>
2021-09-19 23:44:43 +1200

    Add files via upload

---
+++ 06b_input_checking.py
@@ -0,0 +1,26 @@
+# checks if something is valid
+import re
+
+invalid = "yes"
+while invalid == "yes":
+    filename = input("Please enter a filename: ")
+    invalid = "no"
+    valid_char = "[A-Za-z0-9_]"
+
+    for letter in filename:
+        if re.match(valid_char, letter):
+            continue
+        elif letter == " ":
+            error_msg = "No spaces allowed"
+        else:
+            error_msg = "No '{}' is allowed".format(letter)
+        invalid = "yes"
+
+    if filename == "":
+        error_msg = "Filename cannot be blank"
+        invalid = "yes"
+
+    if invalid == "yes":
+        print(error_msg)
+    else:
+        print("Filename {} is valid".format(filename))
